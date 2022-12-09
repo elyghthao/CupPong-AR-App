43b70f9402dcf69402f885003bb99d7ddbccca7c
Chloe Ngo <chloengo1704@gmail.com>
2022-12-08 20:55:07 -0800

    touch screen update

---
+++ CSS451 CupPong/Assets/Elygh Test/TouchScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began)) {
+        //     Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+        //     RaycastHit raycastHit;
+        //     if (Physics.Raycast (raycast, out raycastHit)) {
+        //         Debug.Log ("Something Hit " + raycastHit.collider.name);
+        //     }
+        // }
+    }
+}
